29cedde80aabd967606aeb8b219088be2e8600f3
Tomiris <tomiris9815@gmail.com>
2020-04-07 15:56:10 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+a=5;
+console.log(a);
+var firstNumber=1;
+var string="henlo";
+const third=3;
+var smth;
+
+let tom={
+    name:"Tomiris",
+    age:22,
+    isMarried:false
+};
+console.log(tom["name"]);
+//let answer=prompt("re u tom?");
+console.log(typeof(null));
+console.log(typeof(4+ +"object"));
+console.log(2==="2");
