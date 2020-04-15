fdc7228c53308753dc6d58592592b3195612e943
Tomiris <tomiris9815@gmail.com>
2020-04-15 18:23:41 +0600

    task2

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ let tom={
 //console.log(typeof(null));
 //console.log(typeof(4+ +"object"));
 //console.log(2==="2");
+//tasl1
+/*
 var money=+prompt("Ваш бюджет на месяц?");
 var time=prompt("Введите дату в формате YYYY-MM-DD");
 var expenceName=prompt("Введите обязательную статью расходов в этом месяце");
@@ -32,5 +34,22 @@ var budgetForMonth=expenseSum/30;
 console.log(appData);
 console.log(appData.expenceName + ":" + appData.expenseSum);
 alert("Бюджет на 1 день: " + budgetForMonth);
+*/
+//task2
+//let x = 5; alert( x++ ); 5
+//[ ] + false - null + true NaN +
+//let y = 1; let x = y = 2; alert(x); 2 
+//[ ] + 1 + 2 12
+//alert( "1"[0]) 1
+//2 && 1 && null && 0 && undefined  null
+//2 && 1 && 0 && undefined 0
+//2 && 1 && undefined undefined 
+//!!( a && b ) и (a && b) false?????
+//( null || 2 && 3 || 4 )  3????
+//a = [1, 2, 3]; b = [1, 2, 3]; is a == b false
+//alert( +"Infinity" ); infinity +
+//"ёжик" > "яблоко" true
+//0 || "" || 2 || undefined || true || falsе 
 
+              
 
